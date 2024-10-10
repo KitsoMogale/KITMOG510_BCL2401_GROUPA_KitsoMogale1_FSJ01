cad2997dc01c65fd7cbc6b6e0838b0900219c5a8
Kitso Mogale <kitsomogale19@gmail.com>
2024-10-10 22:45:06 +0200

    .

--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
-const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development', // Disable PWA in development
-  });
-  
-  module.exports = withPWA({
-    // Any other next.js configuration goes here
-  });
+const nextConfig = {};
+
+export default nextConfig;
