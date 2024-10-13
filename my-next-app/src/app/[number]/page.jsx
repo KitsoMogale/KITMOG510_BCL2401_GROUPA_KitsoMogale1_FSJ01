c2b2c1e5711609a508c7b437105a3c27c36c4635
Kitso Mogale <kitsomogale19@gmail.com>
2024-10-13 23:52:03 +0200

    .

--- my-next-app/src/app/[number]/page.jsx
+++ my-next-app/src/app/[number]/page.jsx
@@ -5,7 +5,10 @@ import Sort from '../components/Sort';
 
 export default async function Page({params}) {
   console.log('number1')
-  const getProducts = async (id)=>{
+  const getProducts = async (id=0)=>{
+    // if(id=0){
+
+    // }
       console.log(id,'get')
     let skip = 20*id;
     if(skip<10){
