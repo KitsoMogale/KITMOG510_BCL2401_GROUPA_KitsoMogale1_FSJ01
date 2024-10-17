a8727affbfe7955a9747970f5db938ce67e2792d
Kitso Mogale <kitsomogale19@gmail.com>
2024-10-17 16:15:40 +0200

    .

--- my-next-app/next.config.js
+++ my-next-app/next.config.js
@@ -1,57 +1,13 @@
 const withPWA = require('next-pwa')({
-  dest: 'public',
+  dest: 'public', // Service worker is placed here
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts',
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/.*/i,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'cdnjs-cdn',
-        expiration: {
-          maxEntries: 30,
-        },
-      },
-    },
-    {
-      urlPattern: /^\/api\/.*$/i,
-      handler: 'NetworkFirst',
-      method: 'GET',
-      options: {
-        cacheName: 'api-cache',
-        networkTimeoutSeconds: 10,
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 60 * 60 * 24, // 1 day
-        },
-      },
-    },
-    {
-      urlPattern: /^\/.*\.(?:png|jpg|jpeg|svg|gif|webp)$/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'image-cache',
-        expiration: {
-          maxEntries: 50,
-          maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-        },
-      },
-    },
-  ],
+  disable: process.env.NODE_ENV === 'development', // Disable in development
 });
 
 module.exports = withPWA({
-  // Other Next.js configurations
+  experimental: {
+    appDir: true, // Enable the app directory
+  },
+  // Other next.js configurations
 });
